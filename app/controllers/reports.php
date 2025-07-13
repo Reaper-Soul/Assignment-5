967a2efc942e600e4b85b922cbead5f5f69867d4
Navish Sharma <navishsharma2004@gmail.com>
2025-07-13 19:02:15 +0000

    Created the reports controller to manage the report model and views

--- app/controllers/reports.php
+++ app/controllers/reports.php
@@ -2,10 +2,26 @@
 
 class Reports extends Controller {
 
-    public function index(){
+  public function index(){
+    $_SESSION['current_page'] = 'reports';
+    $this->view('reports/index');
+    die;
+  }
+
+   public function reminders(){
       $_SESSION['current_page'] = 'reports';
-      $this->view('reports/index');
+      $reports = $this->model('Report');
+      $activity = $reports->getRecentActivity(5);
+      $user = $reports->getUserWithMostReminders();
+      $count = $reports->getRemindersCount();
+      $reminders = $reports->getAllReminders();
+      $this->view('reports/reminders', ['activity' => $activity, 'user' => $user, 'count' => $count, 'reminders' => $reminders]);
       die;
     }
 
+   public function user_info(){
+      $_SESSION['current_page'] = 'reports';
+      $this->view('reports/user-info');
+      die;
+   }
 }
